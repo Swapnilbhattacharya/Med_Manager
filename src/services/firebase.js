f52d0cd6456d1ac59f74b071f8752487e27e988f
Swapnilbhattacharya <swapnilmpbfhs@gmail.com>
2026-01-12 23:39:31 +0530

    firebase initialize

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY", // Get these from your Firebase Console tab!
+  authDomain: "YOUR_ID.firebaseapp.com",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
