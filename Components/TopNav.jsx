98e4d81674348a91ea65fa8b0cff972e7a900b62
debendra12345 <79010803+debendra12345@users.noreply.github.com>
2026-01-13 20:41:43 +0530

    Create TopNav.jsx

---
+++ Components/TopNav.jsx
@@ -0,0 +1,14 @@
+export default function TopNav() {
+  return (
+    <div className="top-nav">
+      <h2 className="logo">Medication Manager</h2>
+
+      <div className="nav-links">
+        <button className="active">Dashboard</button>
+        <button>Scan</button>
+        <button>Calendar</button>
+        <button>Profile</button>
+      </div>
+    </div>
+  );
+}
