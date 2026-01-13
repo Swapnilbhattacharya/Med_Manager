5731db8eccfdbe0b7a1a2de7e3eb6c465c9190de
debendra12345 <79010803+debendra12345@users.noreply.github.com>
2026-01-13 23:32:28 +0530

    Create Scan.jsx

---
+++ src/pages/Scan.jsx
@@ -0,0 +1,9 @@
+export default function Scan() {
+  return (
+    <div className="scan">
+      <h3>Scan Medicine</h3>
+      <div className="scanner-box">📷 Camera View</div>
+      <button>Save to Schedule</button>
+    </div>
+  );
+}
