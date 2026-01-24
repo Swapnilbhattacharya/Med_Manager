c3aadfc44cc5c565482b876b6e11f07a75b04133
Swapnilbhattacharya <swapnilmpbfhs@gmail.com>
2026-01-25 00:29:25 +0530

    new API

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = import.meta.env.VITE_GEMINI_KEY; 
+const API_KEY = process.env.REACT_APP_GEMINI_KEY; 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export const getMedicineAlternative = async (userQuery, history = []) => {
